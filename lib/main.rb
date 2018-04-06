40c629525a84457ad5c546c1fecccd31a15cda12
Harjan Knapper <harjanknapper@hotmail.com>
2018-04-06 19:08:47 +0200

    Added obdsim binary for mac.

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,5 @@
 require 'OBD'
 require 'pp'
 
-controller = OBD::M01::Controller.new({:device => '/dev/pts/3'})
-pp controller.get_settings
+controller = OBD::M01::Controller.new({:device => '/dev/ttys2'})
+pp controller.get_vehicle_speed
